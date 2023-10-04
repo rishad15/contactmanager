1a415a8fa2f9ef977d554ef8e584b0fb561317c5
rishad15 <rishad.13khan@gmail.com>
2023-10-04 23:37:58 +0530

    Updates

---
+++ precache-manifest.b554c29e588aae4a342b40feec920e2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c8e9857ecd6edb00dc0",
+    "url": "/contactmanager/static/css/main.c05937d0.chunk.css"
+  },
+  {
+    "revision": "9c8e9857ecd6edb00dc0",
+    "url": "/contactmanager/static/js/main.9c8e9857.chunk.js"
+  },
+  {
+    "revision": "e8e3b76c5bf759905946",
+    "url": "/contactmanager/static/js/1.e8e3b76c.chunk.js"
+  },
+  {
+    "revision": "9b3edc4dda39063d0040",
+    "url": "/contactmanager/static/css/2.d69d0c8e.chunk.css"
+  },
+  {
+    "revision": "9b3edc4dda39063d0040",
+    "url": "/contactmanager/static/js/2.9b3edc4d.chunk.js"
+  },
+  {
+    "revision": "5e8c7c1e1f69d02e0670",
+    "url": "/contactmanager/static/js/runtime~main.5e8c7c1e.js"
+  },
+  {
+    "revision": "5cb910d01a93a64d79e3a7f618bfd45d",
+    "url": "/contactmanager/index.html"
+  }
+];
